2e89f88175682989ce1853436071abb29af72de1
Stephanie Lingwood <stephanielingwood@gmail.com>
2015-03-25 16:45:52 -0700

    cleaned up comments

--- language/Go/pre.sh
+++ language/Go/pre.sh
@@ -18,6 +18,3 @@ apt-get update && apt-get install -y \
 echo "Installing gvm..."
 curl -s -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash;
 echo '[[ -s \"/root/.gvm/scripts/gvm\" ]] && source \"/root/.gvm/scripts/gvm\"' >> /root/.bashrc;
-
-# sudo su - shippable -c "curl -s -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash";
-# sudo su - shippable -c "echo '[[ -s \"/home/shippable/.gvm/scripts/gvm\" ]] && source \"/home/shippable/.gvm/scripts/gvm\"' >> /home/shippable/.bashrc";
