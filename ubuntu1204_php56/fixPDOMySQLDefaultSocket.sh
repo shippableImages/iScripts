b68e25d999aac75ad0c25e34671601b2069be148
nash <nashruddin.amin@gmail.com>
2015-04-02 03:19:58 -0400

    Deleted image

--- ubuntu1204_php56/fixPDOMySQLDefaultSocket.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash -e
-
-sed -i 's/^pdo_mysql.default_socket=$/pdo_mysql.default_socket=\/var\/run\/mysqld\/mysqld.sock/g' /home/shippable/.phpenv/versions/*/etc/php.ini
-sed -i 's/^pdo_mysql.default_socket=$/pdo_mysql.default_socket=\/var\/run\/mysqld\/mysqld.sock/g' /home/shippable/.phpenv/versions/*/etc/.php.ini.original
