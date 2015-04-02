588fd38c71a5373c69ccf3c2ddc6fdeb0f571375
nash <nashruddin.amin@gmail.com>
2015-04-02 03:18:39 -0400

    Deleted image

--- ubuntu1204_php56/restorePHPini.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-sudo su - shippable -c "cp -vr /home/shippable/.phpenv/versions/5.5/etc/.php.ini.original /home/shippable/.phpenv/versions/5.5/etc/php.ini"
-
-echo "=================== Increasing the php mem for all version to 1G ============="
-sed -i s/"memory_limit = 128M"/"memory_limit = 1G"/g /home/shippable/.phpenv/versions/5.5/etc/php.ini
-sed -i s/"memory_limit = 128M"/"memory_limit = 1G"/g /home/shippable/.phpenv/versions/5.5/etc/.php.ini.original
