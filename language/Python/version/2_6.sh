6fc67ab1c9246edbc3f02c2d08753c9cabee154a
Nash <nashruddin.amin@gmail.com>
2015-04-09 21:41:15 +0700

    improve the script

--- language/Python/version/2_6.sh
+++ language/Python/version/2_6.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -e
 
 # Install Python 2.6
-sudo apt-get install -y \
-  python2.6 python2.6-dev \
-  pyopenssl ndg-httpsclient pyasn1
+sudo apt-get install -y python2.6 python2.6-dev pyopenssl ndg-httpsclient pyasn1
 
 # Install virtualenv
 sudo wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O /tmp/ez_setup.py
