431f5dadb11a2f934d036d5b0b94efa4abc93c0b
Nash <nashruddin.amin@gmail.com>
2015-04-10 00:05:50 +0700

    improve the script

--- language/Python/pre.sh
+++ language/Python/pre.sh
@@ -3,4 +3,4 @@
 # install python prereqs
 add-apt-repository -y ppa:fkrull/deadsnakes
 apt-get update
-apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev 
+apt-get install -y libxml2 libxml2-dev libxslt1.1 libxslt1-dev libffi-dev libssl-dev
--- language/Python/version/2_6.sh
+++ language/Python/version/2_6.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 # Install Python 2.6
-sudo apt-get install -y python2.6 python2.6-dev libffi-dev libssl-dev
+sudo apt-get install -y python2.6 python2.6-dev 
 
 # Install virtualenv
 sudo wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O /tmp/ez_setup.py
