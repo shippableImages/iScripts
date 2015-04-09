ba536789b820ad453dd3592c0182f53db0afb541
Nash <nashruddin.amin@gmail.com>
2015-04-09 21:18:59 +0700

    improve the script

--- language/Python/version/2_6.sh
+++ language/Python/version/2_6.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y python2.6 python2.6-dev;
 sudo wget https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py -O /tmp/ez_setup.py
 sudo python2.6 /tmp/ez_setup.py
 sudo easy_install-2.6 pip
-pip install virtualenv
+pip2.6 install virtualenv
 virtualenv -p python2.6 $HOME/venv26
 
 # Install pip packages
