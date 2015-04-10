9c7aad4dcaab81bf6c9b0d1b08e05ef629537709
Nash <nashruddin.amin@gmail.com>
2015-04-10 18:40:44 +0700

    improve the script

--- language/PHP/version/5_3.sh
+++ language/PHP/version/5_3.sh
@@ -8,7 +8,7 @@ apt-get build-dep -y php5-cli
 # Install phpenv
 git clone git://github.com/CHH/phpenv.git $HOME/phpenv
 $HOME/phpenv/bin/phpenv-install.sh
-export PATH=$HOME/.phpenv:$PATH 
+export PATH=$HOME/.phpenv/bin:$PATH 
 eval "$(phpenv init -)" 
 echo 'export PATH=$HOME/.phpenv/bin:$PATH' >> $HOME/.bashrc
 echo 'eval "$(phpenv init -)"' >> $HOME/.bashrc
@@ -18,8 +18,8 @@ git clone git://github.com/php-build/php-build.git $HOME/php-build
 $HOME/php-build/install.sh
 
 # Remove temporary files
-#rm -rf $HOME/phpenv
-#rm -rf $HOME/php-build
+rm -rf $HOME/phpenv
+rm -rf $HOME/php-build
 
 # Compile PHP 5.3.29
 php-build -i development --pear 5.3.29 $HOME/.phpenv/versions/5.3.29
