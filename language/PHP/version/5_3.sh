be416c008d41f6f79cbbcd8f14b40942c11913ec
Nash <nashruddin.amin@gmail.com>
2015-04-10 17:12:52 +0700

    added PHP 5.3

--- language/PHP/version/5_3.sh
+++ language/PHP/version/5_3.sh
@@ -8,9 +8,8 @@ apt-get build-dep -y php5-cli
 # Install phpenv
 git clone git://github.com/CHH/phpenv.git $HOME/phpenv
 $HOME/phpenv/bin/phpenv-install.sh
-echo 'export PATH="/root/.phpenv/bin:$PATH"' >> $HOME/.bashrc
+echo 'export PATH=/root/.phpenv/bin:$PATH' >> $HOME/.bashrc
 echo 'eval "$(phpenv init -)"' >> $HOME/.bashrc
-. $HOME/.bashrc
 
 # Install php-build
 git clone git://github.com/php-build/php-build.git $HOME/php-build
@@ -22,5 +21,5 @@ rm -rf $HOME/php-build
 
 # Compile PHP 5.3.29
 php-build -i development --pear 5.3.29 $HOME/.phpenv/versions/5.3.29
-phpenv rehash
-phpenv global 5.3.29
+source $HOME/.bashrc 
+phpenv rehash && phpenv global 5.3.29
