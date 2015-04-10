6ae12f1dea8a9a3ae33aafb1684c00c03561eb94
Nash <nashruddin.amin@gmail.com>
2015-04-10 18:05:59 +0700

    improve the script

--- language/PHP/version/5_3.sh
+++ language/PHP/version/5_3.sh
@@ -18,11 +18,10 @@ git clone git://github.com/php-build/php-build.git $HOME/php-build
 $HOME/php-build/install.sh
 
 # Remove temporary files
-rm -rf $HOME/phpenv
-rm -rf $HOME/php-build
+#rm -rf $HOME/phpenv
+#rm -rf $HOME/php-build
 
 # Compile PHP 5.3.29
 php-build -i development --pear 5.3.29 $HOME/.phpenv/versions/5.3.29
-source $HOME/.bashrc 
 phpenv rehash 
 phpenv global 5.3.29
