14526d04e90d465ed9f62e9d713d4b1b0394aa7b
Nash <nashruddin.amin@gmail.com>
2015-04-11 21:49:09 +0700

    fixed minor issue

--- language/PHP/pre.sh
+++ language/PHP/pre.sh
@@ -3,24 +3,24 @@
 echo "=========== Installing dependencies ============"
 apt-get update
 apt-get install -y git wget cmake libmcrypt-dev libreadline-dev
-#apt-get build-dep -y php5-cli
+apt-get build-dep -y php5-cli
 
 echo "========== Installing libmemcached =========="
-#wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
-#tar xzf libmemcached-1.0.18.tar.gz && cd libmemcached-1.0.18
-#./configure --enable-sasl
-#make && make install
-#cd .. && rm -fr libmemcached-1.0.18*
+wget https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz
+tar xzf libmemcached-1.0.18.tar.gz && cd libmemcached-1.0.18
+./configure --enable-sasl
+make && make install
+cd .. && rm -fr libmemcached-1.0.18*
 
 echo "=========== Installing librabbitmq ============"
-#wget https://github.com/alanxz/rabbitmq-c/releases/download/v0.5.2/rabbitmq-c-0.5.2.tar.gz
-#tar xzf rabbitmq-c-0.5.2.tar.gz 
-#cd rabbitmq-c-0.5.2 && mkdir build && cd build
-#cmake ..
-#cmake --build . --target install
-#ln -s /usr/local/lib/x86_64-linux-gnu/librabbitmq.so /usr/local/lib/librabbitmq.so
-#ln -s /usr/local/lib/x86_64-linux-gnu/librabbitmq.so.1 /usr/local/lib/librabbitmq.so.1
-#cd ../.. && rm -rf rabbitmq-c*
+wget https://github.com/alanxz/rabbitmq-c/releases/download/v0.5.2/rabbitmq-c-0.5.2.tar.gz
+tar xzf rabbitmq-c-0.5.2.tar.gz 
+cd rabbitmq-c-0.5.2 && mkdir build && cd build
+cmake ..
+cmake --build . --target install
+ln -s /usr/local/lib/x86_64-linux-gnu/librabbitmq.so /usr/local/lib/librabbitmq.so
+ln -s /usr/local/lib/x86_64-linux-gnu/librabbitmq.so.1 /usr/local/lib/librabbitmq.so.1
+cd ../.. && rm -rf rabbitmq-c*
 
 echo "============ Installing phpenv ============="
 git clone git://github.com/CHH/phpenv.git $HOME/phpenv
--- language/PHP/version/5_3.sh
+++ language/PHP/version/5_3.sh
@@ -3,7 +3,6 @@
 echo "============ Building PHP 5.3.29 =============="
 export PATH=$HOME/.phpenv/bin:$PATH
 eval "$(phpenv init -)"
-phpenv
-#php-build -i development --pear 5.3.29 $HOME/.phpenv/versions/5.3.29
-#phpenv rehash 
-#phpenv global 5.3.29
+php-build -i development --pear 5.3.29 $HOME/.phpenv/versions/5.3.29
+phpenv rehash 
+phpenv global 5.3.29
