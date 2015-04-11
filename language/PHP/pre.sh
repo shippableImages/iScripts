2cf350d2e06eb7ce4a6cad64453ddf1cf1d14922
Nash <nashruddin.amin@gmail.com>
2015-04-11 22:46:37 +0700

    fixed minor issue

--- language/PHP/pre.sh
+++ language/PHP/pre.sh
@@ -25,6 +25,8 @@ cd ../.. && rm -rf rabbitmq-c*
 echo "============ Installing phpenv ============="
 git clone git://github.com/CHH/phpenv.git $HOME/phpenv
 $HOME/phpenv/bin/phpenv-install.sh
+echo 'export PATH=$HOME/.phpenv/bin:$PATH' >> $HOME/.bashrc
+echo 'eval "$(phpenv init -)"' >> $HOME/.bashrc
 rm -rf $HOME/phpenv
 
 echo "============ Installing php-build =============="
