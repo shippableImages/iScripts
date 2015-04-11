989a3fb83769285aba6b9af1872e43799113fcd5
Nash <nashruddin.amin@gmail.com>
2015-04-11 21:49:53 +0700

    fixed minor issue

--- language/PHP/post.sh
+++ language/PHP/post.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -e
 
 echo "============ Installing PHPUnit ============="
-#wget https://phar.phpunit.de/phpunit.phar
-#chmod +x phpunit.phar
-#mv phpunit.phar /usr/local/bin/phpunit
+wget https://phar.phpunit.de/phpunit.phar
+chmod +x phpunit.phar
+mv phpunit.phar /usr/local/bin/phpunit
 
 echo "============ Installing Composer ============"
-#curl -s http://getcomposer.org/installer | php
-#chmod +x composer.phar
-#mv composer.phar /usr/local/bin/composer
+curl -s http://getcomposer.org/installer | php
+chmod +x composer.phar
+mv composer.phar /usr/local/bin/composer
 
 echo "=========== Installing PHP extensions =============="
-#apt-get install -y libzmq-dev
-#printf '\n' | pecl install memcache
-#printf '\n' | pecl install memcached
-#printf '\n' | pecl install mongo
-#printf '\n' | pecl install amqp
-#printf '\n' | pecl install zmq-beta
-#printf '\n' | pecl install redis
+apt-get install -y libzmq-dev
+printf '\n' | pecl install memcache
+printf '\n' | pecl install memcached
+printf '\n' | pecl install mongo
+printf '\n' | pecl install amqp
+printf '\n' | pecl install zmq-beta
+printf '\n' | pecl install redis
