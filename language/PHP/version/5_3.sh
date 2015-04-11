b87038922d823b557c103a6127995335fe33d3c5
Nash <nashruddin.amin@gmail.com>
2015-04-11 21:08:33 +0700

    fixed minor issue

--- language/PHP/version/5_3.sh
+++ language/PHP/version/5_3.sh
@@ -2,6 +2,8 @@
 
 echo "============ Building PHP 5.3.29 =============="
 source $HOME/.bashrc
-php-build -i development --pear 5.3.29 $HOME/.phpenv/versions/5.3.29
-phpenv rehash 
-phpenv global 5.3.29
+php-build --definitions
+phpenv
+#php-build -i development --pear 5.3.29 $HOME/.phpenv/versions/5.3.29
+#phpenv rehash 
+#phpenv global 5.3.29
