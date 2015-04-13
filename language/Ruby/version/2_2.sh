785cba0968748fc4e1038f33fa4f2ecd4abe61c4
Nash <nashruddin.amin@gmail.com>
2015-04-13 16:05:59 +0700

    install Ruby 2.2.1

--- language/Ruby/version/2_2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -e
-
-# Activate rvm and install Ruby 2.2
-sudo su - shippable -c 'source /home/shippable/.rvm/scripts/rvm'
-sudo su - shippable -c 'rvm install 2.2'
-sudo su - shippable -c 'rvm use 2.2'
