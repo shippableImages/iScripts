8d734858761bca4234f3c51e424bedba848a3307
Nash <nashruddin.amin@gmail.com>
2015-04-13 21:23:07 +0700

    added install script for OpenJDK6

---
+++ language/Java/version/openjdk6.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+# jdk
+sudo apt-get install -y openjdk-6-jdk
+sudo update-alternatives --set java /usr/lib/jvm/java-6-openjdk-amd64/jre/bin/java
+sudo update-alternatives --set javac /usr/lib/jvm/java-6-openjdk-amd64/bin/javac
+sudo update-alternatives --set javaws /usr/lib/jvm/java-6-openjdk-amd64/jre/bin/javaws
+
+# ant
+
+# maven
+
+# gradle
