29c3fac13eb24e7fee77d0b00b8290222162138d
Nash <nashruddin.amin@gmail.com>
2015-04-13 22:03:39 +0700

    fixed minor issue

--- language/Java/version/openjdk6.sh
+++ language/Java/version/openjdk6.sh
@@ -7,9 +7,12 @@ sudo update-alternatives --set javac /usr/lib/jvm/java-6-openjdk-amd64/bin/javac
 sudo update-alternatives --set javaws /usr/lib/jvm/java-6-openjdk-amd64/jre/bin/javaws
 
 # Set JAVA_HOME variable
-sudo sed '/^JAVA_HOME/d' /etc/environment > /tmp/java_home.txt
-sudo mv /tmp/java_home.txt /etc/environment
-sudo echo 'JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64' >> /etc/environment
+env_file=/etc/profile
+tmp_file=/tmp/env.txt
+sudo sed '/^export JAVA_HOME/d' $env_file > $tmp_file
+sudo echo 'export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64' >> $tmp_file
+sudo mv $tmp_file $env_file
+source $env_file
 
 # ant
 
