fab75948bab2429be865cb8a6000d8a593784009
Nash <nashruddin.amin@gmail.com>
2015-04-13 22:13:20 +0700

    fixed minor issue

--- language/Java/version/openjdk6.sh
+++ language/Java/version/openjdk6.sh
@@ -7,8 +7,8 @@ sudo update-alternatives --set javac /usr/lib/jvm/java-6-openjdk-amd64/bin/javac
 sudo update-alternatives --set javaws /usr/lib/jvm/java-6-openjdk-amd64/jre/bin/javaws
 
 # Set JAVA_HOME variable
-sudo echo 'export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64' >> $HOME/.bash_profile
-sudo echo 'export PATH=$PATH:/usr/lib/jvm/java-6-openjdk-amd64/jre/bin' >> $HOME/.bash_profile
+sudo echo 'export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64' >> $HOME/.bashrc
+sudo echo 'export PATH=$PATH:/usr/lib/jvm/java-6-openjdk-amd64/jre/bin' >> $HOME/.bashrc
 
 # ant
 
