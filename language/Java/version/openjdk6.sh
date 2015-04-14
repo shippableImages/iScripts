2914497528d2c1286d4da9d1d20f4056bd9a6099
Nash <nashruddin.amin@gmail.com>
2015-04-14 07:24:00 +0700

    install apache ant

--- language/Java/version/openjdk6.sh
+++ language/Java/version/openjdk6.sh
@@ -7,10 +7,14 @@ sudo update-alternatives --set javac /usr/lib/jvm/java-6-openjdk-amd64/bin/javac
 sudo update-alternatives --set javaws /usr/lib/jvm/java-6-openjdk-amd64/jre/bin/javaws
 
 # Set JAVA_HOME variable
-sudo echo 'export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64' >> $HOME/.bashrc
-sudo echo 'export PATH=$PATH:/usr/lib/jvm/java-6-openjdk-amd64/jre/bin' >> $HOME/.bashrc
+echo 'export JAVA_HOME=/usr/lib/jvm/java-6-openjdk-amd64' >> $HOME/.bashrc
+echo 'export PATH=$PATH:/usr/lib/jvm/java-6-openjdk-amd64/jre/bin' >> $HOME/.bashrc
 
 # ant
+sudo wget http://mirror.cogentco.com/pub/apache//ant/binaries/apache-ant-1.9.4-bin.tar.gz
+sudo tar xzf apache-ant-1.9.4-bin.tar.gz -C /usr/local && rm -f apache-ant-1.9.4-bin.tar.gz
+echo 'export ANT_HOME=/usr/local/apache-ant-1.9.4' >> $HOME/.bashrc
+echo 'export PATH=$PATH:/usr/local/apache-ant-1.9.4/bin' >> $HOME/.bashrc
 
 # maven
 
