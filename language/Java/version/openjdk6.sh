f200269d2438013b9ee182103dec65870c6a995d
Nash <nashruddin.amin@gmail.com>
2015-04-14 07:30:38 +0700

    install apache maven

--- language/Java/version/openjdk6.sh
+++ language/Java/version/openjdk6.sh
@@ -17,5 +17,8 @@ echo 'export ANT_HOME=/usr/local/apache-ant-1.9.4' >> $HOME/.bashrc
 echo 'export PATH=$PATH:/usr/local/apache-ant-1.9.4/bin' >> $HOME/.bashrc
 
 # maven
+sudo wget http://mirrors.ibiblio.org/apache/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz
+sudo tar xzf apache-maven-3.2.5-bin.tar.gz && rm -f apache-maven-3.2.5-bin.tar.gz
+echo 'export PATH=$PATH:/usr/local/apache-maven-3.2.5/bin' >> $HOME/.bashrc
 
 # gradle
