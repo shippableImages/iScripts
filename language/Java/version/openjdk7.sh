a1f65555914cb37b63df8868899cb3f7a2ac7218
Nash <nashruddin.amin@gmail.com>
2015-04-14 13:49:47 +0700

    install maven 3.3.1

--- language/Java/version/openjdk7.sh
+++ language/Java/version/openjdk7.sh
@@ -13,8 +13,8 @@ sudo tar xzf apache-ant-1.9.4-bin.tar.gz -C /usr/local && rm -f apache-ant-1.9.4
 echo 'export ANT_HOME=/usr/local/apache-ant-1.9.4' >> $HOME/.bashrc
 
 # Install Apache Maven
-sudo wget http://mirrors.ibiblio.org/apache/maven/maven-3/3.2.5/binaries/apache-maven-3.2.5-bin.tar.gz
-sudo tar xzf apache-maven-3.2.5-bin.tar.gz -C /usr/local && rm -f apache-maven-3.2.5-bin.tar.gz
+sudo wget http://mirror.symnds.com/software/Apache/maven/maven-3/3.3.1/binaries/apache-maven-3.3.1-bin.tar.gz 
+sudo tar xzf apache-maven-3.3.1-bin.tar.gz -C /usr/local && rm -f apache-maven-3.3.1-bin.tar.gz
 
 # Install Gradle
 sudo wget https://services.gradle.org/distributions/gradle-2.3-all.zip
@@ -23,5 +23,5 @@ unzip -qq gradle-2.3-all.zip -d /usr/local && rm -f gradle-2.3-all.zip
 # Update system path
 echo 'export PATH=$PATH:/usr/lib/jvm/java-7-openjdk-amd64/jre/bin' >> $HOME/.bashrc
 echo 'export PATH=$PATH:/usr/local/apache-ant-1.9.4/bin' >> $HOME/.bashrc
-echo 'export PATH=$PATH:/usr/local/apache-maven-3.2.5/bin' >> $HOME/.bashrc
+echo 'export PATH=$PATH:/usr/local/apache-maven-3.3.1/bin' >> $HOME/.bashrc
 echo 'export PATH=$PATH:/usr/local/gradle-2.3/bin' >> $HOME/.bashrc
