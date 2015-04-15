d946dc1daacb854e78848f70f61d8a5d55dca8b4
Nash <nashruddin.amin@gmail.com>
2015-04-15 11:46:31 +0700

    simplify the install scripts for Java

--- apt-get/language/Java/version/oraclejdk7.sh
+++ apt-get/language/Java/version/oraclejdk7.sh
@@ -10,3 +10,19 @@ sudo update-alternatives --set javac /usr/lib/jvm/java-7-oracle/bin/javac
 sudo update-alternatives --set javaws /usr/lib/jvm/java-7-oracle/jre/bin/javaws
 echo 'export JAVA_HOME=/usr/lib/jvm/java-7-oracle' >> $HOME/.bashrc
 echo 'export PATH=$PATH:/usr/lib/jvm/java-7-oracle/jre/bin' >> $HOME/.bashrc
+
+# Install Apache Ant
+sudo wget http://mirror.cogentco.com/pub/apache//ant/binaries/apache-ant-1.9.4-bin.tar.gz
+sudo tar xzf apache-ant-1.9.4-bin.tar.gz -C /usr/local && rm -f apache-ant-1.9.4-bin.tar.gz
+echo 'export ANT_HOME=/usr/local/apache-ant-1.9.4' >> $HOME/.bashrc
+echo 'export PATH=$PATH:/usr/local/apache-ant-1.9.4/bin' >> $HOME/.bashrc
+
+# Install Apache Maven
+sudo wget http://mirror.symnds.com/software/Apache/maven/maven-3/3.3.1/binaries/apache-maven-3.3.1-bin.tar.gz 
+sudo tar xzf apache-maven-3.3.1-bin.tar.gz -C /usr/local && rm -f apache-maven-3.3.1-bin.tar.gz
+echo 'export PATH=$PATH:/usr/local/apache-maven-3.3.1/bin' >> $HOME/.bashrc
+
+# Install Gradle
+sudo wget https://services.gradle.org/distributions/gradle-2.3-all.zip
+unzip -qq gradle-2.3-all.zip -d /usr/local && rm -f gradle-2.3-all.zip
+echo 'export PATH=$PATH:/usr/local/gradle-2.3/bin' >> $HOME/.bashrc
