1533bbe7af8a8f45812953bb0c5815f274b85a94
Nash <nashruddin.amin@gmail.com>
2015-04-15 20:01:29 +0700

    activate rvm on starting the shell

--- apt-get/language/Ruby/version/1_9_3.sh
+++ apt-get/language/Ruby/version/1_9_3.sh
@@ -3,7 +3,8 @@
 # Install rvm
 gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
 curl -sSL https://get.rvm.io | bash -s stable
-source /etc/profile.d/rvm.sh
+echo '[[ -s /etc/profile.d/rvm.sh ]] && source /etc/profile.d/rvm.sh' >> $HOME/.bashrc
+[[ -s /etc/profile.d/rvm.sh ]] && source /etc/profile.d/rvm.sh
 
 # Activate rvm and install Ruby 1.9.3
 rvm requirements
