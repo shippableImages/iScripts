7be9b1b2a9fab112bf47b67dba53a15ec95fe308
Nash <nashruddin.amin@gmail.com>
2015-04-15 10:26:21 +0700

    install Go tip

--- apt-get/language/Go/version/Tip.sh
+++ apt-get/language/Go/version/Tip.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-echo "Installing needed packages..."
 # TODO: cross-check this list against final ubuntu os script
 # Install packages for Go
 apt-get update && apt-get install -y \
@@ -11,22 +10,15 @@ apt-get update && apt-get install -y \
 	mercurial \
 	cmake \
 	scons \
-	binutils \
+	binutils
 
-echo "Installing gvm..."
-curl -s -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash;
-echo '[[ -s \"/root/.gvm/scripts/gvm\" ]] && source \"/root/.gvm/scripts/gvm\"' >> /root/.bashrc;
+# Install gvm
+curl -s -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash
+[[ -s "$HOME/.gvm/scripts/gvm" ]] && source $HOME/.gvm/scripts/gvm 
 
-# Install Go tip
-echo "Installing Go tip..."
-. /root/.gvm/scripts/gvm && gvm install tip;
+# Get latest version
+version=$(gvm listall | xargs | awk 'NF>1{print $NF}')
 
-# Activate Go
-[[ -s "/root/.gvm/scripts/gvm" ]] && source "/root/.gvm/scripts/gvm"
-
-gvm use tip --default
-
-export GVM_ROOT="/root/.gvm"
-export PATH="$PATH:/root/.gvm/bin"
-
-echo "Done installing Go tip"
+# Install Go
+gvm install $version && gvm use $version --default
+echo 'GVM_ROOT=$HOME/.gvm' >> $HOME/.bashrc
