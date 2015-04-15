186bb9157ffda74f5c8a55ef02126a8acfca2d37
Nash <nashruddin.amin@gmail.com>
2015-04-15 09:31:01 +0700

    removed dead code

--- apt-get/language/Go/version/1_1.sh
+++ apt-get/language/Go/version/1_1.sh
@@ -17,22 +17,3 @@ curl -s -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-i
 [[ -s "$HOME/.gvm/scripts/gvm" ]] && source $HOME/.gvm/scripts/gvm 
 gvm install go1.1 && gvm use go1.1 --default
 echo 'GVM_ROOT=$HOME/.gvm' >> $HOME/.bashrc
-
-
-#echo "Installing gvm..."
-#curl -s -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash;
-#echo '[[ -s \"/root/.gvm/scripts/gvm\" ]] && source \"/root/.gvm/scripts/gvm\"' >> /root/.bashrc;
-#
-# Install Go 1.1
-#echo "Installing Go 1.1..."
-#. /root/.gvm/scripts/gvm && gvm install go1.1
-#
-# Activate Go
-#[[ -s "/root/.gvm/scripts/gvm" ]] && source "/root/.gvm/scripts/gvm"
-#
-#gvm use go1.1 --default
-#
-#export GVM_ROOT="/root/.gvm"
-#export PATH="$PATH:/root/.gvm/bin"
-#
-#echo "Done installing Go 1.1"
