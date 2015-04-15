acfac4765ea83e68636259650d52c54e6cfd743f
Nash <nashruddin.amin@gmail.com>
2015-04-15 10:38:20 +0700

    install scripts for Go 1.2, 1.3, and 1.4

--- apt-get/language/Go/version/1_3.sh
+++ apt-get/language/Go/version/1_3.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-echo "Installing needed packages..."
 # TODO: cross-check this list against final ubuntu os script
 # Install packages for Go
 apt-get update && apt-get install -y \
@@ -11,22 +10,10 @@ apt-get update && apt-get install -y \
 	mercurial \
 	cmake \
 	scons \
-	binutils \
+	binutils
 
-echo "Installing gvm..."
-curl -s -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash;
-echo '[[ -s \"/root/.gvm/scripts/gvm\" ]] && source \"/root/.gvm/scripts/gvm\"' >> /root/.bashrc;
-
-# Install Go 1.3
-echo "Installing Go 1.3..."
-. /root/.gvm/scripts/gvm && gvm install go1.3;
-
-# Activate Go
-[[ -s "/root/.gvm/scripts/gvm" ]] && source "/root/.gvm/scripts/gvm"
-
-gvm use go1.3 --default
-
-export GVM_ROOT="/root/.gvm"
-export PATH="$PATH:/root/.gvm/bin"
-
-echo "Done installing Go 1.3"
+# Install Go 1.3.3
+curl -s -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash
+[[ -s "$HOME/.gvm/scripts/gvm" ]] && source $HOME/.gvm/scripts/gvm 
+gvm install go1.3.3 && gvm use go1.3.3 --default
+echo 'GVM_ROOT=$HOME/.gvm' >> $HOME/.bashrc
--- apt-get/language/Go/version/1_4.sh
+++ apt-get/language/Go/version/1_4.sh
@@ -1,6 +1,5 @@
 #!/bin/bash -e
 
-echo "Installing needed packages..."
 # TODO: cross-check this list against final ubuntu os script
 # Install packages for Go
 apt-get update && apt-get install -y \
@@ -11,22 +10,10 @@ apt-get update && apt-get install -y \
 	mercurial \
 	cmake \
 	scons \
-	binutils \
+	binutils
 
-echo "Installing gvm..."
-curl -s -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash;
-echo '[[ -s \"/root/.gvm/scripts/gvm\" ]] && source \"/root/.gvm/scripts/gvm\"' >> /root/.bashrc;
-
-# Install Go 1.4
-echo "Installing Go 1.4..."
-. /root/.gvm/scripts/gvm && gvm install go1.4;
-
-# Activate Go
-[[ -s "/root/.gvm/scripts/gvm" ]] && source "/root/.gvm/scripts/gvm"
-
-gvm use go1.4 --default
-
-export GVM_ROOT="/root/.gvm"
-export PATH="$PATH:/root/.gvm/bin"
-
-echo "Done installing Go 1.4"
+# Install Go 1.4.2
+curl -s -L https://raw.githubusercontent.com/moovweb/gvm/master/binscripts/gvm-installer | bash
+[[ -s "$HOME/.gvm/scripts/gvm" ]] && source $HOME/.gvm/scripts/gvm 
+gvm install go1.4.2 && gvm use go1.4.2 --default
+echo 'GVM_ROOT=$HOME/.gvm' >> $HOME/.bashrc
