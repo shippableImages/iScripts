2a42fb273eb9d3d81ccc8a8003c5844f481d23e3
Nash <nashruddin.amin@gmail.com>
2015-04-21 07:55:15 +0700

    install cassandra 2.0

--- apt-get/service/Cassandra/version/2_0.sh
+++ apt-get/service/Cassandra/version/2_0.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -e
-# placeholder for Cassandra 2.0
-true
+
+# Install Cassandra 2.0
+sudo echo "deb http://debian.datastax.com/community stable main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+sudo curl -L http://debian.datastax.com/debian/repo_key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install -y dsc20=2.0.11-1 cassandra=2.0.11
+
+# Start service on boot
+sudo update-rc.d cassandra defaults
