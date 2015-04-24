d606546f5567bd2c1a89a569e346eda43f3e59a3
Nash <nashruddin.amin@gmail.com>
2015-04-24 09:31:07 +0700

    install ElasticSearch 1.5

---
+++ apt-get/service/ElasticSearch/version/1_5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+VERSION=1.5.1
+
+# Install ElasticSearch
+sudo wget https://download.elastic.co/elasticsearch/elasticsearch/elasticsearch-${VERSION}.tar.gz
+sudo tar xzf elasticsearch-${VERSION}.tar.gz -C /usr/local && sudo rm -f elasticsearch-${VERSION}.tar.gz
+sudo ln -s /usr/local/elasticsearch-${VERSION}/bin/elasticsearch /usr/local/bin/elasticsearch
+
+# Start elasticsearch
+sudo elasticsearch &
+
+# Make sure service is running
+curl http://localhost:9200
