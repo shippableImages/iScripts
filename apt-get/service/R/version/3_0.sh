64980317accc68ca5123eeab3787604a318733a4
Nash <nashruddin.amin@gmail.com>
2015-04-25 17:16:31 +0700

    install R 2.x and 3.x

---
+++ apt-get/service/R/version/3_0.sh
@@ -0,0 +1,19 @@
+#!/bin/bash -e
+
+VERSION=3.0.3
+
+# Install prerequisites
+sudo apt-get install -y gfortran libreadline-dev
+
+# Install OpenJDK7 if no Java interpreter exist
+jre=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+if [[ -z "$jre" ]]; then
+  sudo apt-get install -y openjdk-7-jre
+fi
+
+# Install R
+sudo wget http://cran.cnr.berkeley.edu/bin/linux/ubuntu/trusty/r-base_${VERSION}.orig.tar.gz
+sudo tar xzf r-base_${VERSION}.orig.tar.gz && cd R-${VERSION}
+./configure --without-x
+make && sudo make install
+cd .. && sudo rm -rf R-${VERSION} && sudo rm -f r-base_${VERSION}.orig.tar.gz
