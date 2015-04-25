457a35a0464048a06e1838d044aa9339061987d1
Nash <nashruddin.amin@gmail.com>
2015-04-25 20:39:53 +0700

    install ghc 7.10, 7.8, 7.6, 7.4

---
+++ apt-get/language/Haskell/version/7_10.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+# Install ghc
+printf '\n' | sudo add-apt-repository ppa:hvr/ghc
+sudo apt-get update
+sudo apt-get install -y cabal-install-1.22 ghc-7.10.1
+
+# Update path
+echo 'export PATH=/opt/cabal/1.22/bin:/opt/ghc/7.10.1/bin:$PATH' >> $HOME/.bashrc
+. $HOME/.bashrc
