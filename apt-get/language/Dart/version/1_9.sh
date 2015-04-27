9458acf09ee420d524e552d1132f2d1a36c4d472
Nash <nashruddin.amin@gmail.com>
2015-04-27 08:26:17 +0700

    install Dart latest, 1.9, 1.8, 1.7, 1.6

---
+++ apt-get/language/Dart/version/1_9.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -e
+
+# Install Dart
+sudo wget https://storage.googleapis.com/dart-archive/channels/stable/release/45104/sdk/dartsdk-linux-x64-release.zip
+sudo unzip -qq dartsdk-linux-x64-release.zip -d /usr/local
+sudo rm -f dartsdk-linux-x64-release.zip
+echo 'export PATH=$PATH:/usr/local/dart-sdk/bin' >> $HOME/.bashrc
---
+++ apt-get/language/Dart/version/latest.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+# Pre-installation setup
+sudo apt-get install apt-transport-https
+sudo sh -c 'curl https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -'
+sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_stable.list > /etc/apt/sources.list.d/dart_stable.list'
+sudo sh -c 'curl https://storage.googleapis.com/download.dartlang.org/linux/debian/dart_unstable.list > /etc/apt/sources.list.d/dart_unstable.list'
+sudo apt-get update
+
+# Install Dart
+sudo apt-get install -y dart
