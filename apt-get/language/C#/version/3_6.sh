4a32a486a0fc36c61180895f67327c4df484b7b3
Nash <nashruddin.amin@gmail.com>
2015-04-29 16:24:45 +0700

    install C# 3.4, 3.6, 3.8, 3.10, 3.12, and 4.0

---
+++ apt-get/language/C#/version/3_6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -e
+
+VERSION=3.6.0
+
+# Install dependencies
+sudo apt-get install -y bzip2 gettext
+
+# Install mono
+sudo wget http://download.mono-project.com/sources/mono/mono-${VERSION}.tar.bz2
+sudo bunzip2 mono-${VERSION}.tar.bz2 && sudo tar xf mono-${VERSION}.tar
+cd mono-${VERSION}
+sudo ./configure --prefix=/usr/local
+sudo make && sudo make install
+cd .. && sudo rm -rf mono-${VERSION}*
