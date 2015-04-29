55790e922c4bea6febfce7e8c2014a0e78107b7b
Nash <nashruddin.amin@gmail.com>
2015-04-29 21:00:17 +0700

    added install scripts for Erlang

---
+++ apt-get/language/Erlang/version/R16B01.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+VERSION=R16B01
+
+# Install dependencies
+sudo apt-get install -y libssl-dev libncurses5-dev
+
+# Install Erlang
+sudo wget https://packages.erlang-solutions.com/erlang/esl-erlang-src/otp_src_${VERSION}.tar.gz
+tar xzf otp_src_${VERSION}.tar.gz && cd otp_src_${VERSION}
+export ERL_TOP=`pwd`
+export LANG=C
+./configure --without-java
+make && sudo make install
+cd .. && sudo rm -rf otp_src_${VERSION}*
