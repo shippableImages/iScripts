0788fb1044415004c3a0c75850aac97703be979b
Nash <nashruddin.amin@gmail.com>
2015-04-30 11:46:43 +0700

    added install scripts for Groovy

---
+++ apt-get/language/Groovy/version/2_2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+# Make sure Java is installed
+java_version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
+if [[ -z "$java_version" ]]; then
+  echo "Groovy needs Java to be installed on your system. Exiting."
+  exit 1
+fi
+
+# Install groovy
+curl -s http://get.gvmtool.net | bash
+source $HOME/.gvm/bin/gvm-init.sh
+printf 'y\n' | gvm install groovy 2.2.2
