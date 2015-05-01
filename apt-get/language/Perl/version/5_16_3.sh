022cc6130d18844accf2aabb41c9e49caf4673d4
Nash <nashruddin.amin@gmail.com>
2015-05-01 19:31:39 +0700

    added install scripts for Perl

---
+++ apt-get/language/Perl/version/5_16_3.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+VERSION=5.16.3
+
+# Install Perl
+curl -L http://install.perlbrew.pl | bash
+echo 'source ~/perl5/perlbrew/etc/bashrc' >> $HOME/.bashrc
+source ~/perl5/perlbrew/etc/bashrc
+perlbrew install perl-${VERSION}
+perlbrew switch perl-${VERSION}
